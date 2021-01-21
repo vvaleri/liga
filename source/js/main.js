19a846110bcb0db70274f6095691a2ae59c419cb
Valeria Rodionova <valeriya_off@mail.ru>
2021-01-21 06:07:48 +0300

    Add: menu burger

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 
 import {initModals} from './modules/init-modals';
 import {clickLoader} from './modules/preloader';
+import {openMenu} from './modules/burger';
 
 // Utils
 // ---------------------------------
@@ -15,4 +16,5 @@ iosVhFix();
 
 initModals();
 clickLoader();
+openMenu();
 
---
+++ source/js/modules/burger.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+
+const openMenu = () => {
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+        document.body.classList.toggle('lock');
+    })
+}
+
+export {openMenu}
